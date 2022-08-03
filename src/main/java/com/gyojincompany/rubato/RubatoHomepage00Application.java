6e42ca5439569bc4ab8e4b6ea5e3a004d72deafa
ATE <ATE@DESKTOP-ODIPDNU>
2022-08-03 14:10:10 +0900

    루바토 홈페이지 프로젝트 jsp 만들기

---
+++ src/main/java/com/gyojincompany/rubato/RubatoHomepage00Application.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.rubato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubatoHomepage00Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubatoHomepage00Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyojincompany/rubato/RubatoHomepage00ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.rubato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubatoHomepage00ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
